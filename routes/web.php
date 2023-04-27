1d9ed39d4b29cd7243d8cf18e811e63d24395ede
Muhamad Riyanto <98597500+muhamadriyanto21@users.noreply.github.com>
2023-04-27 01:49:40 -0400

    first commit meet with team

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+// tes
 
 Route::get('/', function () {
     return view('welcome');
